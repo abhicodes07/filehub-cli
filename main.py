60abcb35dae5f8d0503fb6358d4fa358ecd131e9
Abhinav <abhiasthana76@gmail.com>
2026-06-17 14:11:05 +0530

    feat: :sparkles: display files in fzf viewer

--- main.py
+++ main.py
@@ -4,29 +4,14 @@ import sys
 
 
 def get_repository_url():
+    """Take the repository URL as command line argument"""
     repo_url = sys.argv[1:]
-    repo_contents = repo_url[0].split("/")
-    return repo_contents
-
-
-def fetch_files(repo_content):
-    files = []
-    type = []
-    for content in repo_content:
-        files.append(content["name"])
-        type.append((content["name"], content["type"]))
-
-    # print(content.keys())
-    # print(files)
-    # print(type)
-
-    subprocess.run(
-        f"{files} | fzf --prompt=' Select file >> ' --height=~50% --layout=reverse --border --exit-0",
-        shell=True,
-    )
+    repo_info = repo_url[0].split("/")
+    return repo_info
 
 
 def get_repository_content():
+    """Fetch the repository contents and metadata"""
     repo = get_repository_url()
     repo_owner = repo[3]
     repo_name = repo[4]
@@ -42,10 +27,75 @@ def get_repository_content():
     return repo_content_json
 
 
+def choose_file(file_list, prompt):
+    """Display list of files in fzf viewer."""
+    if not file_list:
+        return None
+
+    try:
+        result = subprocess.run(
+            [
+                "fzf",
+                "--prompt",
+                prompt,
+                "--height",
+                "~50%",
+                "--layout",
+                "reverse",
+                "--border",
+                "--exit-0",
+            ],
+            capture_output=True,
+            text=True,
+            check=True,
+            input=file_list,
+        )
+    except ChildProcessError:
+        print("No file selected, Aborting!")
+        return None
+
+    return result
+
+
+def fetch_files(repo_content):
+    """
+    Get files and its contents
+
+    repo_content: list
+    """
+    if not repo_content:
+        return None
+
+    files = []
+    dirs = []
+
+    for content in repo_content:
+        if content["type"] == "file":
+            files.append(content["name"])
+        else:
+            dirs.append(content["name"])
+        # files.append(content["name"])
+        # file_types.append((content["name"], content["type"]))
+
+    print(files)
+    print(dirs)
+
+
+def download_file(file_name, repo_content):
+    if not file_name:
+        return None
+
+    file = file_name.split("\n")
+    for item in repo_content:
+        if item["name"] == file[0]:
+            print("found")
+        else:
+            print("not found")
+
+
 def main():
     content = get_repository_content()
     fetch_files(content)
-    # print(content[4].items())
 
 
 if __name__ == "__main__":
